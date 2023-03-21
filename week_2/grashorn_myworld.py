54f19f31fcaad9708d8dd7e5eecad6d4b569800f
Brett Grashorn <brettgrashorn@gmail.com>
2023-03-20 21:52:33 -0500

    Week 2

---
+++ week_2/grashorn_myworld.py
@@ -0,0 +1,8 @@
+""" 
+    Title: grashorn-myworld.py
+    Author: Brett Grashorn
+    Date: 20 March 2023
+    Description: File for Assignment 2.3. Creating first
+    Python environment.
+"""
+print("You are now in Grashorn's World!")
