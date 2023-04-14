678f380c881ab80dabc542af963264091de9651c
Brett Grashorn <brettgrashorn@gmail.com>
2023-04-13 20:54:32 -0500

    Projections add

---
+++ week_5/grashorn-projections.js
@@ -0,0 +1,28 @@
+
+/**
+	Title: projections.js
+    Author: Brett Grashorn
+    Date: 4/13/23
+    Description: MongoDB Shell Scripts for the users collection.
+    Work Cited:
+    Web 335 Assign_5
+    Web 335 GitHub Examples
+ */
+
+// a query to add a new user
+db.users.insert({
+    firstName: "Max",
+    lastName: "Richter",
+    employeeId: "1227",
+    email: "maxrich@email.com",
+    dateCreated: new Date(),
+  });
+  
+  // a query to update Mozarts email
+  db.users.update({ employeeId: "1009" }, { $set: { email: "mozart@me.com" } });
+  
+  //prove it worked
+  db.users.find({ lastName: "Mozart" });
+  
+  // a query that lists all the documents within the collection with projections
+  db.users.find({}, { firstName: 1, lastName: 1, email: 1 });
